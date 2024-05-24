6134e20fb4ce21163d54d7ff66500af1f6ae4c93
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2024-05-24 14:34:31 +0530

    Update __main__.py

--- Minato/__main__.py
+++ Minato/__main__.py
@@ -1,16 +1,1 @@
-from telegram import Update, constants, InlineKeyboardButton, InlineKeyboardMarkup
 
-from info import dispatcher
-from telegram.ext import CommandHandler, ContextTypes
-
-from telegram.utils.helpers import mention_markdown
-
-async def yup(update: Update, context):
-
-    user_id = update.effective_user.id
-    user_name = update.effective_user.first_name
-    mention = mention_markdown(user_id=user_id, name=user_name, version=2)
-
-start_handler = CommandHandler('yup', yup)
-
-dispatcher.add_handler(start_handler)
